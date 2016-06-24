2d9dce04f941567a6dff0375a36c5cebdee13361
RabbirBR <rabbirbr1@gmail.com>
2016-06-24 15:05:06 +0600

    Added User Migrationons
    1. Users Migrations Modified.
    2. Added create_admins_table migration.
    3. Added create_employee_table migration.
    4. Modified HomeController to check various roles.

---
+++ database/migrations/2016_06_24_085606_create_admins_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAdminsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('admins', function (Blueprint $table) {
+            $table->integer('id');
+            $table->date('joining_date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('admins');
+    }
+}
